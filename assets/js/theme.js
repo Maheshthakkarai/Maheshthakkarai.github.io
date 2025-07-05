604b6736283473de9756dbfd191bf02599902a6c
Maheshthakkarai <maheshthakkarai@gmail.com>
2025-07-05 14:44:15 -0400

    Add search and category filter to WebApp Gallery.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,38 +1,36 @@
-(function() {
-    // Function to apply the theme
-    function applyTheme(theme) {
-        if (theme === 'dark') {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    }
-
-    // Immediately apply the theme on initial load
-    let savedTheme = localStorage.getItem('color-theme');
-    if (savedTheme) {
-        applyTheme(savedTheme);
+// Function to apply the theme
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
     } else {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            applyTheme('dark');
-        }
+        document.documentElement.classList.remove('dark');
     }
+}
 
-    // Add listener after the DOM is loaded to handle the toggle button
-    document.addEventListener('DOMContentLoaded', () => {
-        const themeToggleBtn = document.getElementById('theme-toggle');
-        if (themeToggleBtn) {
-            // Set the toggle's initial state based on the current theme
-            if (document.documentElement.classList.contains('dark')) {
-                themeToggleBtn.checked = true;
-            }
+// Immediately apply the theme on initial load
+let savedTheme = localStorage.getItem('color-theme');
+if (savedTheme) {
+    applyTheme(savedTheme);
+} else {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        applyTheme('dark');
+    }
+}
 
-            // Add event listener for the toggle
-            themeToggleBtn.addEventListener('change', function() {
-                const theme = this.checked ? 'dark' : 'light';
-                localStorage.setItem('color-theme', theme);
-                applyTheme(theme);
-            });
+// Add listener for the toggle button after the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    if (themeToggleBtn) {
+        // Set the toggle's initial state based on the current theme
+        if (document.documentElement.classList.contains('dark')) {
+            themeToggleBtn.checked = true;
         }
-    });
-})();
+
+        // Add event listener for the toggle
+        themeToggleBtn.addEventListener('change', function() {
+            const theme = this.checked ? 'dark' : 'light';
+            localStorage.setItem('color-theme', theme);
+            applyTheme(theme);
+        });
+    }
+});
