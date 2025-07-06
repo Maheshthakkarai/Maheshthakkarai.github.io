8f6f8013a07259c232fdf1127b0ce8f0878ccf73
Maheshthakkarai <maheshthakkarai@gmail.com>
2025-07-06 19:37:42 -0400

    update firebase config

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,12 +1,9 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOCAbC123dEf456gHiJkLmNoPqRsTuVwX",
-  authDomain: "my-portfolio-blog-12345.firebaseapp.com",
-  projectId: "my-portfolio-blog-12345",
-  storageBucket: "my-portfolio-blog-12345.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890abcdef"
+  apiKey: "AIzaSyBzxw5W3Qm768dnfnSseCr7lPeZULKG5VI",
+  authDomain: "my-portfolio-blog-408c2.firebaseapp.com",
+  projectId: "my-portfolio-blog-408c2",
+  storageBucket: "my-portfolio-blog-408c2.appspot.com",
+  messagingSenderId: "1031785439115",
+  appId: "1:1031785439115:web:30866c81e63c271671198e"
 };
-
-// Initialize Firebase
-// Note: The Firebase SDKs will be included in the HTML files.
