4d420636005b255b8d0b05033b23e8d5f35f6172
Maheshthakkarai <maheshthakkarai@gmail.com>
2025-07-06 20:56:19 -0400

    Blog section removal

--- assets/js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBzxw5W3Qm768dnfnSseCr7lPeZULKG5VI",
-  authDomain: "my-portfolio-blog-408c2.firebaseapp.com",
-  projectId: "my-portfolio-blog-408c2",
-  storageBucket: "my-portfolio-blog-408c2.appspot.com",
-  messagingSenderId: "1031785439115",
-  appId: "1:1031785439115:web:30866c81e63c271671198e"
-};
